fb54d1e2830a18b7d223a106db99cb98a27140f0
yuki321-alt <yuki.ifsp@gmail.com>
2024-12-05 12:48:25 -0300

    v1 do meu código
    v1 do meu código

---
+++ Restaurante/js/cardapio.js
@@ -0,0 +1,12 @@
+const massas = document.getElementById('massas');
+
+const doces = document.getElementById('doces');
+
+function aparecerMassas() {
+    massas.classList.toggle('visu');
+   
+}
+
+function aparecerDoces() {
+    doces.classList.toggle('visu');
+}
